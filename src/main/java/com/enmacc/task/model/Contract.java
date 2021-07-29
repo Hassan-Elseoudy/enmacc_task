6f5e194a6855b67a5794de6987ef044a9efad123
helseoudy <hassan.elseoudy@cegedim.com>
2021-07-29 22:12:13 +0200

    Change Generation type to IDENTITY and update graph.

--- src/main/java/com/enmacc/task/model/Contract.java
+++ src/main/java/com/enmacc/task/model/Contract.java
@@ -12,7 +12,7 @@ import java.util.UUID;
 public class Contract {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne( fetch = FetchType.LAZY)
--- src/main/java/com/enmacc/task/model/Graph.java
+++ src/main/java/com/enmacc/task/model/Graph.java
@@ -1,4 +1,28 @@
 package com.enmacc.task.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Graph {
+
+    private Integer v;
+    private ArrayList[] adjList;
+
+    public Graph(Integer vertices) {
+        this.v = vertices;
+        this.adjList = new ArrayList[this.v];
+        Arrays.stream(this.adjList).forEach(adj -> adj = new ArrayList<>());
+    }
+
+    public void addEdge(Integer u, Integer v) {
+        adjList[u].add(v);
+    }
+
 }
