8ae167904ada4d3cccbd49101c15b9e2a1b6da9b
helseoudy <hassan.elseoudy@cegedim.com>
2021-07-30 08:21:09 +0200

    New packaging for util, with error handling.

--- src/main/java/com/enmacc/task/util/SpringFoxConfig.java
+++ src/main/java/com/enmacc/task/util/SpringFoxConfig.java
@@ -1,4 +1,4 @@
-package com.enmacc.task.controller;
+package com.enmacc.task.util;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
