d63d21f3425d998ce97388a5dff4bad39038a2fb
helseoudy <hassan.elseoudy@cegedim.com>
2021-08-01 13:20:56 +0200

    Add validates tests.

--- src/test/java/com/enmacc/task/controller/ContractControllerTest.java
+++ src/test/java/com/enmacc/task/controller/ContractControllerTest.java
@@ -3,27 +3,36 @@ package com.enmacc.task.controller;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class ContractControllerTest extends EnmaccApplicationTest {
 
     @LocalServerPort
     private int port;
 
+    @Autowired
+    public MockMvc mockMvc;
+
     @BeforeAll
-    public static void beforeAll() {
+    static void beforeAll() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
     }
 
     @BeforeEach
     public void setup() {
-
         RestAssured.port = port;
         super.setUp();
     }
@@ -34,38 +43,52 @@ public class ContractControllerTest extends EnmaccApplicationTest {
     }
 
 
-//    @Test
-//    public void addContract() {
-//        //language=JSON
-//        String json = "{\n" +
-//                "  \"fromCompany\": \"1\",\n" +
-//                "  \"toCompany\": \"2\"\n" +
-//                "}";
-//
-//        //@formatter:off
-//        given()
-//                .contentType(ContentType.JSON)
-//                .body(json)
-//                .when()
-//                .post("contracts")
-//                .then()
-//                .statusCode(HttpStatus.SC_CREATED);
-//        //@formatter:on
-//    }
-//
-//    @Test
-//    public void getAllPossibleSleeves() {
-//
-//        //@formatter:off
-//        given()
-//                .contentType(ContentType.JSON)
-//                .param("fromCompany", "1")
-//                .param("toCompany", "2")
-//                .when()
-//                .get("sleeves")
-//                .then()
-//                .statusCode(HttpStatus.SC_OK);
-//        //@formatter:on
-//
-//    }
+    @Test
+    public void addContract() {
+        //language=JSON
+        String json = "{\n" +
+                "  \"fromCompany\": \"1\",\n" +
+                "  \"toCompany\": \"2\"\n" +
+                "}";
+
+        //@formatter:off
+        given()
+                .contentType(ContentType.JSON)
+                .body(json)
+                .when()
+                .post("contracts")
+                .then()
+                .statusCode(HttpStatus.SC_CREATED);
+        //@formatter:on
+    }
+
+    @Test
+    public void getAllContractsShouldReturn3() throws Exception {
+        // Act
+        mockMvc.perform(MockMvcRequestBuilders.
+                get("/contracts")
+                .contentType(MediaType.APPLICATION_JSON))
+                // Assert
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(3)));
+    }
+
+
+
+    @Test
+    @Order(3)
+    public void getAllPossibleSleeves() {
+        //@formatter:off
+        given()
+                .contentType(ContentType.JSON)
+                .param("aCompany", "1")
+                .param("bCompany", "2")
+                .when()
+                .get("sleeves")
+                .then()
+                .statusCode(HttpStatus.SC_OK);
+        //@formatter:on
+
+    }
 }
--- src/test/java/com/enmacc/task/controller/EnmaccApplicationTest.java
+++ src/test/java/com/enmacc/task/controller/EnmaccApplicationTest.java
@@ -1,11 +1,16 @@
 package com.enmacc.task.controller;
 
+import com.enmacc.task.model.Company;
+import com.enmacc.task.model.Contract;
 import com.enmacc.task.repository.CompanyRepository;
+import com.enmacc.task.repository.ContractRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
 
 import static com.enmacc.task.controller.MockData.getCompanies;
 
@@ -20,12 +25,21 @@ abstract public class EnmaccApplicationTest {
     @Autowired
     public CompanyRepository companyRepository;
 
+    @Autowired
+    public ContractRepository contractRepository;
+
     void setUp() {
         companyRepository.deleteAll();
-        companyRepository.saveAll(getCompanies());
+        List<Company> companies = getCompanies();
+        companies = companyRepository.saveAll(companies);
+        contractRepository.save(new Contract(1L, companies.get(1), companies.get(4)));
+        contractRepository.save(new Contract(2L, companies.get(1), companies.get(3)));
+        contractRepository.save(new Contract(3L, companies.get(4), companies.get(2)));
+
     }
 
     void tearDown() {
+        contractRepository.deleteAll();
         companyRepository.deleteAll();
     }
 
